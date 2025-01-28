4625d89415ccf323fd941b58ff2afeec32bc39c5
heric-hh <erhh.015@gmail.com>
2025-01-28 13:57:39 -0600

    proyecto terminado

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Guitar from "./components/Guitar";
+import { db } from "./data/guitarras.js";
+import { useState } from "react";
+export default function App() {
+  const [guitars, setGuitars] = useState(db);
+  const [carrito, setCarrito] = useState([]);
+
+  function agregarCarrito(guitar) {
+    console.log(`agregando al carrito ${guitar.id}, ${guitar.nombre}`);
+    const carritoNuevo = [...carrito];
+    const idExiste = carritoNuevo.findIndex((g) => g.id === guitar.id);
+    if (idExiste === -1) {
+      carritoNuevo.push({ ...guitar, cantidad: 1 });
+    } else {
+      carritoNuevo[idExiste].cantidad++;
+    }
+    setCarrito(carritoNuevo);
+  }
+
+  function quitarUno(id) {
+    const carritoNuevo = [...carrito];
+    const idCarrito = carritoNuevo.findIndex((guitar) => guitar.id === id);
+    if (carritoNuevo[idCarrito].cantidad > 1) {
+      carritoNuevo[idCarrito].cantidad--;
+    }
+    setCarrito(carritoNuevo);
+  }
+
+  function quitarGuitarra(id) {
+    console.log(`Quitar guitarra ${id}`);
+    setCarrito(carrito.filter((guitar) => guitar.id !== id));
+  }
+
+  function vaciarCarrito() {
+    setCarrito([]);
+  }
+  return (
+    <>
+      <Header
+        carrito={carrito}
+        agregarCarrito={agregarCarrito}
+        quitarUno={quitarUno}
+        quitarGuitarra={quitarGuitarra}
+        vaciarCarrito={vaciarCarrito}
+      />
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+          {guitars.map((guitar) => (
+            <Guitar
+              key={guitar.id}
+              guitar={guitar}
+              agregarCarrito={agregarCarrito}
+            />
+          ))}
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+}
